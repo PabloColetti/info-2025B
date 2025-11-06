3e508700631d3266146f41b897a7a97eacea4edc
pabloinformatorio <pabloinformatorio@gmail.com>
2025-11-06 09:13:47 -0300

    Add usuario_id column to posts table and update user status management

--- CLASE14/blog.sql
+++ CLASE14/blog.sql
@@ -4,6 +4,7 @@ CREATE TABLE posts (
 	titulo VARCHAR(200) NOT NULL,
 	contenido TEXT NOT NULL,
 	fecha_publicacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	-- usuario_id INT NOT NULL REFERENCES usuarios(id)
 );
 
 SELECT * FROM posts;
@@ -27,5 +28,39 @@ VALUES
 	('Pablo', 'pablo@emailfalso.com', 'facil123'),
 	('Elias', 'elias@emailfalso.com', 'refacil123');
 
+ALTER TABLE posts
+ADD COLUMN usuario_id INT REFERENCES usuarios(id);
+
+ALTER TABLE posts
+DROP COLUMN usuario_id;
+
+SELECT * FROM posts;
+
+UPDATE posts
+SET usuario_id = 1
+WHERE id = 3;
+
+INSERT INTO posts (titulo, contenido)
+VALUES
+	('Tercer post', 'Contenido del tercer post');
+
+ALTER TABLE posts
+ALTER COLUMN usuario_id SET NOT NULL;
+
+INSERT INTO posts (titulo, contenido, usuario_id)
+VALUES
+	('Cuarto post', 'Contenido del cuarto post', 2);
+
+
+
+
+SELECT * FROM usuarios;
+
+CREATE TYPE user_status_enum AS ENUM ('ACTIVE', 'INACTIVE');
+
+ALTER TABLE usuarios
+ADD COLUMN estado user_status_enum DEFAULT 'ACTIVE';
+
+ALTER TYPE user_status_enum ADD VALUE 'PENDING';
 
 
